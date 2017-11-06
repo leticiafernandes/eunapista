1c43547ceb5df39a0098c660c648a87e5f20ac1b
Leticia <leticia@macbook-pro-60.corp.globoi.com>
2017-11-06 14:22:08 -0200

    calendar

--- src/RaceScreen/RaceCalendar.js
+++ src/RaceScreen/RaceCalendar.js
@@ -3,8 +3,40 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
 import { Container, Header, Body, Content, Title, Icon, List, ListItem, Left } from 'native-base';
 import AppConfig from '../config';
+import axios from 'axios';
+import moment from 'moment';
 
 export default class RaceCalendar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markedDates: []
+    }
+  }
+
+  componentWillMount = () => {
+    axios.get(`${AppConfig.host}/events.json`)
+    .then(response => {
+      let markedDates = [];
+
+      response
+        .data
+        .map(event => event)
+        .map(event => {
+          markedDates.push({
+            date: [moment(new Date(event.start_date)).format('YYYY-MM-DD')]
+          });
+        });
+
+      this.setState({markedDates: markedDates});
+    }).catch((error)=>{
+      console.log("Api call error");
+      alert(error.message);
+    });
+  }
+
   render() {
     LocaleConfig.locales['br'] = {
       monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
@@ -31,10 +63,18 @@ export default class RaceCalendar extends React.Component {
             onMonthChange={(month) => {console.log('month changed', month)}}
             hideExtraDays={true}
             firstDay={1}
+            markedDates={
+              this.state.markedDates.map(markedDate => {
+                `'${markedDate.date}':{marked:true},`
+                console.log(`'${markedDate.date}':{marked:true},`);
+              })
+            }
             theme={{
               textSectionTitleColor: AppConfig.primaryColor,
               arrowColor: AppConfig.primaryColor,
               monthTextColor: AppConfig.primaryColor,
+              selectedDayBackgroundColor: '#dasdsa',
+              selectedDayTextColor: '#f0f0f0'
             }}
           />
           <View style={styles.box1}>
